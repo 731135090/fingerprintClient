df10b87b70bfc61e8437fbc0d649ae62f7cebbde
731135090 <731135090@qq.com>
2019-09-05 09:12:54 +0800

    file md5

--- util/file/file.go
+++ util/file/file.go
@@ -1,6 +1,10 @@
 package file
 
 import (
+	"bufio"
+	"crypto/md5"
+	"encoding/hex"
+	"io"
 	"os"
 )
 
@@ -14,4 +18,24 @@ func FileSize(fileName string) int {
 		return int(info.Size())
 	}
 	return 0
+}
+
+/**
+ * 获取文件md5
+ * @param string fileName file name
+ * @return （string, error）
+ */
+func FileMd5(fileName string) (string, error) {
+	fileObj, err := os.OpenFile(fileName, os.O_RDONLY, 0644)
+	if err != nil {
+		return "", err
+	}
+	defer fileObj.Close()
+	reader := bufio.NewReaderSize(fileObj, 1024*1024*1)
+	md5 := md5.New()
+	_, err = io.Copy(md5, reader)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(md5.Sum(nil)), nil
 }
